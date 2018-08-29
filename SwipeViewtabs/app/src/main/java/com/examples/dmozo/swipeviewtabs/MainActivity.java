36cd07a55805d61832917f1bf98c46d19836ef6e
Daniel Eduardo Mozo Pabon <dmozo@asesoftware.com>
2018-08-29 15:07:56 -0500

    Cambios minimos

--- SwipeViewtabs/app/src/main/java/com/examples/dmozo/swipeviewtabs/MainActivity.java
+++ SwipeViewtabs/app/src/main/java/com/examples/dmozo/swipeviewtabs/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -68,29 +70,6 @@ public class MainActivity extends AppCompatActivity implements BlueFragment.OnFr
             }
         });
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in androidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -173,6 +152,8 @@ public class MainActivity extends AppCompatActivity implements BlueFragment.OnFr
                     return "AZUL";
             }return null;
         }
+
+
 
 
     }
